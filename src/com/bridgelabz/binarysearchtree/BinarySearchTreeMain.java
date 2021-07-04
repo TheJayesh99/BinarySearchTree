9dd9d2c4ad2f7352e279a38e2f042229a40a334b
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-04 19:10:55 +0530

    Added : approach to create binary tree shown in figure

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -4,9 +4,26 @@ public class BinarySearchTreeMain
 {
 	public static void main(String[] args)
 	{
-		BinarySearchTreeService BST = new BinarySearchTreeService();
-		BST.insert(56);
-		BST.insert(30);
-		BST.insert(70);
+//		uc1 - create binary search tree
+		BinarySearchTreeService BST1 = new BinarySearchTreeService();
+		BST1.insert(56);
+		BST1.insert(30);
+		BST1.insert(70);
+		
+//		uc2 - create a binary tree as shown in figure
+		BinarySearchTreeService BST2 = new BinarySearchTreeService();
+		BST2.insert(56);
+		BST2.insert(30);
+		BST2.insert(22);
+		BST2.insert(11);
+		BST2.insert(3);
+		BST2.insert(16);
+		BST2.insert(40);
+		BST2.insert(70);
+		BST2.insert(95);
+		BST2.insert(60);
+		BST2.insert(65);
+		BST2.insert(63);
+		BST2.insert(67);
 	}
 }
